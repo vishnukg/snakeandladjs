02581b9278cecf1b1b84675bce762dd53517fb82
Vishnu Ganesan <vishnukg@gmail.com>
2023-01-26 00:43:55 +1100

    Added board config

--- src/game/config/board.js
+++ src/game/config/board.js
@@ -1,17 +1,19 @@
-export const obstacles = {
-    4: {
-        type: 'ladder',
-        move: 8,
-        direction: 'forward'
-    },
-    7: {
-        type: 'snake',
-        move: 3,
-        direction: 'backward'
-    },
-    10: {
-        type: 'ladder',
-        move: 4,
-        direction: 'forward'
-    }
+export default () => {
+    return {
+        4: {
+            type: 'ladder',
+            move: 8,
+            direction: 'forward'
+        },
+        7: {
+            type: 'snake',
+            move: 3,
+            direction: 'backward'
+        },
+        10: {
+            type: 'ladder',
+            move: 4,
+            direction: 'forward'
+        }
+    };
 };
