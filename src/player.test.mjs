46d4605035fb4e08caba8d7c5bd132806669f5da
Vishnu Ganesan <vishnukg@gmail.com>
2024-03-22 23:24:36 +1100

    Lazy

--- src/player.test.mjs
+++ src/player.test.mjs
@@ -1,94 +1,50 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import { rollDie } from './game/roll/index.mjs';
-
-const createGame = (firstPlayer) => {
-    let winner;
-    let started = false;
-
-    const move = (player) => {
-        if (!started && player !== firstPlayer) {
-            return -1;
-        }
-        const pos = player.move();
-        if (!winner && player.finished) winner = player;
-        return pos;
-    };
-
-    return {
-        move,
-        get winner() {
-            return winner;
-        }
-    };
-};
-
-const create_player = () => {
-    let position = 0;
-    let finished = false;
-
-    const moveOne = () => {
-        position++;
-        if (position === 100) finished = true;
-        return position;
-    };
-
-    const move = (n = 1) => {
-        for (let i = 0; i < n; i++) {
-            moveOne();
-        }
-        return position;
-    };
-
-    return {
-        move,
-        get finished() {
-            return finished;
-        }
-    };
-};
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import { rollDie } from './game/roll/index.mjs'
+import { createGame } from './game.mjs'
+import { create_player } from './player.mjs'
 
 test('Player moves one position starting at zero', () => {
-    const player = create_player();
-    const newPosition = player.move();
-    assert.equal(newPosition, 1);
-});
+  const player = create_player()
+  const newPosition = player.move()
+  assert.equal(newPosition, 1)
+})
 
 test('Player moves twice to reach position 2', () => {
-    const player = create_player();
-    player.move();
-    const newPosition = player.move();
-    assert.equal(newPosition, 2);
-});
+  const player = create_player()
+  player.move()
+  const newPosition = player.move()
+  assert.equal(newPosition, 2)
+})
 
 test('Player moves twice to reach position 2', () => {
-    const player = create_player();
-    player.move();
-    const newPosition = player.move();
-    assert.equal(newPosition, 2);
-});
+  const player = create_player()
+  player.move()
+  const newPosition = player.move()
+  assert.equal(newPosition, 2)
+})
 
 test('Player moves 100 times to finish the game', () => {
-    const player = create_player();
-    const numberOfMoves = 100;
-    while (player.move() < numberOfMoves);
-    assert.equal(player.finished, true);
-});
+  const player = create_player()
+  const numberOfMoves = 100
+  while (player.move() < numberOfMoves);
+  assert.equal(player.finished, true)
+})
 
 test('Player would not finish the game if they dont reach position 100', () => {
-    const player = create_player();
-    const numberOfMoves = 99;
-    while (player.move() < numberOfMoves) {
-        assert.equal(player.finished, false);
-    }
-});
+  const player = create_player()
+  const numberOfMoves = 99
+  while (player.move() < numberOfMoves) {
+    assert.equal(player.finished, false)
+  }
+})
 
 test('Player moves by the value returned by the die', () => {
-    const player = create_player();
-    const numberOfMoves = rollDie();
-    const newPosition = player.move(numberOfMoves);
-    assert.equal(newPosition, numberOfMoves);
-});
+  const player = create_player()
+  const numberOfMoves = rollDie()
+  const newPosition = player.move(numberOfMoves)
+  assert.equal(newPosition, numberOfMoves)
+})
 
 // test('Of two players, the player 2 reaches 100 to win the game', () => {
 //     const p1 = create_player();
@@ -103,11 +59,11 @@ test('Player moves by the value returned by the die', () => {
 // });
 
 test('Of two players, second player cannot make the first move', () => {
-    const p1 = create_player();
-    const p2 = create_player();
+  const p1 = create_player()
+  const p2 = create_player()
 
-    const game = createGame(p1);
+  const game = createGame(p1)
 
-    const result = game.move(p2);
-    assert.equal(result, -1);
-});
+  const result = game.move(p2)
+  assert.equal(result, -1)
+})
