53399470f1b7c659ce1febb2acafdaf4c0f45755
Vishnu Ganesan <vishnukg@gmail.com>
2024-03-29 14:08:36 +1100

    changes

--- src/game.mjs
+++ src/game.mjs
@@ -1,20 +1,20 @@
 export const createGame = (firstPlayer) => {
-  let winner
-  let started = false
+    let winner
+    let started = false
 
-  const move = (player) => {
-    if (!started && player !== firstPlayer) {
-      return -1
+    const move = (player) => {
+        if (!started && player !== firstPlayer) {
+            return -1
+        }
+        const pos = player.move()
+        if (!winner && player.finished) winner = player
+        return pos
     }
-    const pos = player.move()
-    if (!winner && player.finished) winner = player
-    return pos
-  }
 
-  return {
-    move,
-    get winner() {
-      return winner
-    },
-  }
+    return {
+        move,
+        get winner() {
+            return winner
+        },
+    }
 }
